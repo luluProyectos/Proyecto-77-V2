1f0304d8aadd8983f019a8a8ca7ae24c4ad12764
luluProyectos <105942783+luluProyectos@users.noreply.github.com>
2022-08-03 14:18:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+console.log("Mi primer proyecto en la consola");
+
+            var primer_nombre = "Lourdes ";
+            var apellido = "Acuayte";
+
+            var nombre_completo = primer_nombre.concat(apellido);
+            console.log("Nombre completo: "+nombre_completo);
+
+            var a = 10;
+            var b = 5;
+
+            var sumar = a + b ;
+            console.log("Suma: "+sumar);
+
+            var restar = a - b ;
+            console.log("Resta: "+restar);
+
+            var multiplicar = a * b ;
+            console.log("Multiplicación: "+multiplicar);
+
+            var dividir = a / b ;
+            console.log("División: "+dividir);
